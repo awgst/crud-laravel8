da4d39c3feef466985ed1709feb18a3e0c9be1f9
Awang Suria Trisakti <60491935+awgst@users.noreply.github.com>
2021-08-11 07:43:11 +0700

    Updates routes

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,17 +13,17 @@
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="{{ url('/') }}">Navbar</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link active" aria-current="page" href="{{ url('/') }}">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+                <a class="nav-link" href="{{ url('about') }}">About</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Product</a>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index', ['title'=>'Home']);
 });
+Route::get('about', function () {
+    return view('about', ['title'=>'About']);
+});
